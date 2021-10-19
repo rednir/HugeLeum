65022c8c3d9221873cf8d2c05beb3254444b546b
lucaadams <lucaads2006@gmail.com>
2021-10-19 20:28:01 +0100

    added script to main meny

---
+++ UI/MainMenu.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	$PlayButton.connect("pressed", self, "on_play_button_pressed")
+
+func on_play_button_pressed():
+	if get_tree().change_scene("Gameplay/Game.tscn") != OK:
+		pass # handle error
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
