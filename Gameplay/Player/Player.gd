e90ffe1febca8ae2996de0cc5b5e9feb0716e4e6
lucaadams <lucaads2006@gmail.com>
2021-10-20 18:24:19 +0100

    added horizontal player movement

--- Gameplay/Player/Player.gd
+++ Gameplay/Player/Player.gd
@@ -1,12 +1,14 @@
 extends KinematicBody2D
 
 
+export var horizontal_move_speed = 400
+export var horizontal_move_force = 50
 export var weight = 20
 export var jump_height = 300
 export var additional_jump_force = 30 # when jump key held
 export var max_jump_key_hold_time = 0.15
 
-var y_velocity = 0
+var velocity = Vector2(0, 0)
 var jumping = true
 var jump_key_held = false
 var jump_key_hold_time = 0
@@ -19,7 +21,7 @@ func _ready():
 
 func _input(event):
 	if event.is_action_pressed("jump") and not jumping:
-		y_velocity -= jump_height
+		velocity.y -= jump_height
 		jumping = true
 		jump_key_held = true
 	
@@ -29,18 +31,31 @@ func _input(event):
 
 
 func _process(delta):
+	if Input.is_action_pressed("move_left"):
+		if velocity.x > -horizontal_move_speed:
+			velocity.x -= horizontal_move_force
+	else:
+		if velocity.x < 0:
+			velocity.x += horizontal_move_force
+
+	if Input.is_action_pressed("move_right"):
+		if velocity.x < horizontal_move_speed:
+			velocity.x += horizontal_move_force
+	else:
+		if velocity.x > 0:
+			velocity.x -= horizontal_move_force
+
 	if jump_key_held and jump_key_hold_time < max_jump_key_hold_time:
 		jump_key_hold_time += delta
-		print("adding to jump")
 	elif jump_key_hold_time > max_jump_key_hold_time:
 		jump_key_held = false
-		print("set to false")
 
 	if jumping:
 		if jump_key_held:
-			y_velocity -= additional_jump_force
-		y_velocity += weight
-		collision_info = move_and_collide(Vector2(0, y_velocity * delta))
-		if collision_info:
-			jumping = false
-			y_velocity = 0
+			velocity.y -= additional_jump_force
+		velocity.y += weight
+
+	collision_info = move_and_collide(Vector2(velocity.x * delta, velocity.y * delta))
+	if collision_info:
+		jumping = false
+		velocity.y = 0
