181e287de06a3ba3f0a29dbaa0b655a572652455
Davran Dilshat <davran.dilshat@gmail.com>
2021-10-21 22:43:02 +0100

    move bg and ground in menu screen

--- UI/MainMenu.gd
+++ UI/MainMenu.gd
@@ -2,6 +2,8 @@ extends Control
 
 
 onready var fade = $Fade
+onready var ground_parallax = $Ground/ParallaxBackground
+onready var bg_parallax = $Background/ParallaxBackground
 
 
 func _ready():
@@ -12,6 +14,11 @@ func on_play_button_pressed():
 	fade.play("in")
 	fade.connect("animation_finished", self, "play", ["in"])
 	
+
+func _process(delta):
+	ground_parallax.scroll_offset -= Vector2(20, 0) * delta
+	bg_parallax.scroll_offset -= Vector2(20, 0) * delta
+
 
 func play(_a, _b):
 	get_tree().change_scene("Gameplay/Game.tscn")
