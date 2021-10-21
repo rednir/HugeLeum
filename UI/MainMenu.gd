0f88dfdc0730f34e9dafd61c2f54357d56b7e4d7
Davran Dilshat <davran.dilshat@gmail.com>
2021-10-21 19:13:26 +0100

    add fade out in menu

--- UI/MainMenu.gd
+++ UI/MainMenu.gd
@@ -1,9 +1,17 @@
 extends Control
 
 
+onready var animation_player = $AnimationPlayer
+
+
 func _ready():
 	$PlayButton.connect("pressed", self, "on_play_button_pressed")
 
 
 func on_play_button_pressed():
+	animation_player.play("out")
+	animation_player.connect("animation_finished", self, "play", ["out"])
+	
+
+func play(_a, _b):
 	get_tree().change_scene("Gameplay/Game.tscn")
