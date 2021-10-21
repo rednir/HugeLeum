028fd4d7fb1bc5668c68a22e10559d8043b4ed32
Davran Dilshat <davran.dilshat@gmail.com>
2021-10-21 17:06:11 +0100

    revert your shit

--- Gameplay/Ground/Ground.gd
+++ Gameplay/Ground/Ground.gd
@@ -1,6 +1,5 @@
 extends StaticBody2D
 
-
 const screen_width = 1024
 
 const plains_texture = preload("res://Assets/plains-ground.png")
@@ -9,27 +8,10 @@ const desert_texture = preload("res://Assets/desert-ground.png")
 onready var animation_player = $AnimationPlayer
 onready var main_sprite = $MainSprite
 onready var other_sprite = $OtherSprite
-onready var camera = $Camera
-
-var camera_starting_pos_x
-var camera_distance_travelled_x = 0
-var camera_last_pos_x
 
 
 func _ready():
 	pass
-
-
-func _process(_delta):
-	if camera_starting_pos_x == null:
-		camera_starting_pos_x = camera.position.x
-
-	camera_distance_travelled_x += camera.get_camera_position().x - camera_last_pos_x
-	print(camera_distance_travelled_x)
-
-	if camera_distance_travelled_x > screen_width * 2:
-		translate(Vector2(screen_width * 2, 0))
-		print("translated")
 
 
 func _on_enviornment_change(env: int):
