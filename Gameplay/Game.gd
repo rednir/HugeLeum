1fd6886cd994e95073b00073a1545b55b61856be
lucaadams <lucaads2006@gmail.com>
2021-10-21 16:49:52 +0100

    started adding ground movement

--- Gameplay/Game.gd
+++ Gameplay/Game.gd
@@ -1,13 +1,12 @@
 extends Node2D
 
 
-var scroll_speed = 50
+var scroll_speed = 100
 
 onready var camera = $Camera
 
 
 func _ready():
-	print("game ready")
 	camera.set_scroll_speed(scroll_speed)
 
 
