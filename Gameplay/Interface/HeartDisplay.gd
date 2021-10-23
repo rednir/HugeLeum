5bbaa6f996fb000dc6e2ca70c6bbad463ed3001d
Davran Dilshat <davran.dilshat@gmail.com>
2021-10-23 21:43:55 +0100

    improve heart display logic

--- Gameplay/Interface/HeartDisplay.gd
+++ Gameplay/Interface/HeartDisplay.gd
@@ -10,7 +10,7 @@ export var max_hearts = 3
 export var heart_count = 2
 
 
-func _ready():
+func initialize():
 	for _i in range(heart_count):
 		var heart = TextureRect.new()
 		heart.texture = heart_full_texture
@@ -22,33 +22,25 @@ func _ready():
 		container.add_child(heart)
 
 
-func set_hearts(num: int):
-	num = max_hearts if num > max_hearts else num
-
-	if num > heart_count:
-		add_hearts(num)
-	elif num < heart_count:
-		remove_hearts(num)
-
-	heart_count = num
-
-
-func add_hearts(num: int):
+func set_hearts(new_heart_count: int):
 	var children = container.get_children()
-	if children.size() > 0:
-		for _i in range(num - heart_count):
+	if children.size() == 0:
+		initialize()
+
+	new_heart_count = max_hearts if new_heart_count > max_hearts else new_heart_count
+
+	if new_heart_count > heart_count:
+		for _i in range(new_heart_count - heart_count):
 			children[children.size() - 1].queue_free()
-
-	for _i in range(num - heart_count):
-		var heart = TextureRect.new()
-		heart.texture = heart_full_texture
-		container.add_child(heart)
-
-
-func remove_hearts(num: int):
-	var children = container.get_children()
-	if children.size() > 0:
-		for i in range(heart_count - num):
-			children[heart_count - 1 - i].texture = heart_empty_texture
 	
-	animation_player.play("loss")
+		for _i in range(new_heart_count - heart_count):
+			var heart = TextureRect.new()
+			heart.texture = heart_full_texture
+			container.add_child(heart)
+	elif new_heart_count < heart_count:
+		for i in range(heart_count - new_heart_count):
+			children[heart_count - 1 - i].texture = heart_empty_texture
+		animation_player.play("loss")
+
+	heart_count = new_heart_count
+
