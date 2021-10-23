d4b7943c1328d397892563ba471efd4d989faa15
Davran Dilshat <davran.dilshat@gmail.com>
2021-10-23 23:40:53 +0100

    fix eagle not moving

--- Gameplay/Entities/Eagle.gd
+++ Gameplay/Entities/Eagle.gd
@@ -1,11 +1,13 @@
 extends KinematicBody2D
 
+export var move_speed = 100
+
 var disappear = false
 var extra_movement = 0
 
 
 func _process(delta):
 	extra_movement += 19 if disappear else 0
-	position.x -= (extra_movement + extra_movement) * delta
+	position.x -= (extra_movement + move_speed) * delta
 	if extra_movement > 2000:
 		queue_free()
