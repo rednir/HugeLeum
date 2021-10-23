708e88148fc04613a73be3f95ac91d0d299397af
lucaadams <lucaads2006@gmail.com>
2021-10-23 12:16:06 +0100

    remove debug prints

--- Gameplay/Interface/Results.gd
+++ Gameplay/Interface/Results.gd
@@ -4,8 +4,6 @@ extends CenterContainer
 signal main_menu_pressed
 signal play_again_pressed
 
-var nullll = false
-
 
 func _ready():
 	$Panel/MainMenuButton.connect("pressed", self, "on_main_menu_button_pressed")
@@ -15,16 +13,8 @@ func _ready():
 func set_display_stats(score=0, jump_count=0, powerup_count=0):
 	var score_label = $Panel/Score
 	var info_label = $Panel/InfoLabel
-
-	if score_label == null:
-		nullll = true
-		
-	var score_label_text = str(int(round(score))) + "m"
-	print(score_label_text)
-	score_label.text = score_label_text
-	var info_label_text = "Total jumps: " + str(jump_count) + "\nPowerups collected: " + str(powerup_count)
-	print(info_label_text)
-	info_label.text = info_label_text
+	score_label.text = str(int(round(score))) + "m"
+	info_label.text = "Total jumps: " + str(jump_count) + "\nPowerups collected: " + str(powerup_count)
 
 
 func on_main_menu_button_pressed():
