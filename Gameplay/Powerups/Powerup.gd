3ed7f4094ea605f5c459aca24ed790750118b3b4
Davran Dilshat <davran.dilshat@gmail.com>
2021-10-23 17:51:37 +0100

    add animation to powerups

--- Gameplay/Powerups/Powerup.gd
+++ Gameplay/Powerups/Powerup.gd
@@ -1,6 +1,9 @@
 extends RigidBody2D
 
 
+onready var animation_player = $AnimationPlayer
+
+
 func _ready():
 	pass
 
@@ -11,3 +14,9 @@ func _process(_delta):
 
 func on_pickup():
 	get_child(0).set_deferred("disabled", true)
+	animation_player.play("pickup")
+	animation_player.connect("animation_finished", self, "on_animation_finished", ["pickup"])
+
+
+func on_animation_finished(_a, _b):
+	queue_free()
