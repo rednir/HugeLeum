3a9dc5ca7705c7c495886bcad37ecc3fd821eaa1
Davran Dilshat <davran.dilshat@gmail.com>
2021-10-23 23:44:30 +0100

    fix bee not moving

--- Gameplay/Entities/Bee.gd
+++ Gameplay/Entities/Bee.gd
@@ -1,11 +1,13 @@
 extends StaticBody2D
 
+export var move_speed = 100
+
 var disappear = false
 var extra_movement = 0
 
 
 func _process(delta):
 	extra_movement += 38 if disappear else 0
-	position.x -= extra_movement * delta
+	position.x -= (move_speed + extra_movement) * delta
 	if extra_movement > 2000:
 		queue_free()
