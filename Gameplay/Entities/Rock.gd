eb87a14ba3d5dfc1e8468dac568266624b4489c0
Davran Dilshat <davran.dilshat@gmail.com>
2021-10-23 23:43:26 +0100

    simplify code

--- Gameplay/Entities/Rock.gd
+++ Gameplay/Entities/Rock.gd
@@ -5,7 +5,7 @@ var extra_movement = 0
 
 
 func _process(delta):
-	extra_movement += 19 if disappear else 0
-	position.x -= (extra_movement + extra_movement) * delta
+	extra_movement += 38 if disappear else 0
+	position.x -= extra_movement * delta
 	if extra_movement > 2000:
 		queue_free()
