259d6a9317c2f19c071d9cdc09d45a1b8ab96475
Davran Dilshat <davran.dilshat@gmail.com>
2021-10-24 14:43:51 +0100

    add animation to buttons

--- UI/Button/Button.gd
+++ UI/Button/Button.gd
@@ -1,14 +1,25 @@
 extends Button
 
-
 onready var audio_stream_player = $AudioStreamPlayer
+onready var animation_player = $AnimationPlayer
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.connect("pressed", self, "_on_pressed")
+	self.connect("button_down", self, "_on_down")
+	self.connect("button_up", self, "_on_up")
+	
+	rect_pivot_offset = rect_size / 2
 
 
 func _on_pressed():
 	audio_stream_player.play()
-	
+
+
+func _on_down():
+	animation_player.play("down")
+
+
+func _on_up():
+	animation_player.play("up")
