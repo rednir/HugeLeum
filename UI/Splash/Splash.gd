91e270b5646c4a231d670c2cbc4aac5ef8c061cd
Davran Dilshat <davran.dilshat@gmail.com>
2021-10-25 18:50:25 +0100

    update splash + ability to skip splash

--- UI/Splash/Splash.gd
+++ UI/Splash/Splash.gd
@@ -7,6 +7,11 @@ onready var animation_player = $AnimationPlayer
 func _ready():
 	animation_player.connect("animation_finished", self, "_on_animation_finished", ["show"])
 
+
+func _input(event):
+	if event.is_action_pressed("ui_accept") or event.is_action_pressed("ui_cancel"):
+		_on_animation_finished(null, null)
+
 	
 func _on_animation_finished(_a, _b):
 	get_tree().change_scene("UI/MainMenu.tscn")
