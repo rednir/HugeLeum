7cd86b96f12d7cf39448524d37bbe4dbba87cb47
Davran Dilshat <davran.dilshat@gmail.com>
2021-10-26 12:42:19 +0100

    add toggle for touch controls in settings

--- UI/SettingsMenu.gd
+++ UI/SettingsMenu.gd
@@ -3,7 +3,9 @@ extends CanvasLayer
 
 onready var animation_player = $CenterContainer/AnimationPlayer
 onready var done_button = $CenterContainer/Panel/DoneButton
+
 onready var master_volume_slider = $CenterContainer/Panel/MasterVolume/Slider
+onready var touch_controls_button = $CenterContainer/Panel/TouchControls/CheckButton
 
 
 func _ready():
@@ -11,6 +13,9 @@ func _ready():
 
 	master_volume_slider.connect("value_changed", self, "on_master_volume_changed")
 	master_volume_slider.value = AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master"))
+
+	touch_controls_button.connect("toggled", self, "on_touch_controls_toggled")
+	touch_controls_button.disabled = Settings.FORCE_TOUCH_CONTROLS
 
 
 func on_done_button_pressed():
@@ -25,3 +30,7 @@ func on_animation_finished(_a):
 func on_master_volume_changed(value):
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), value)
 	AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), value <= master_volume_slider.min_value)
+
+
+func on_touch_controls_toggled(value: bool):
+	Settings.show_touch_controls = value
